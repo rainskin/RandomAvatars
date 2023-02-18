bec8b2ff2d49720e98157092ad6a5715b204f220
levch <levchenko.d.a1998@gmail.com>
2023-02-18 07:36:23 +0600

    v 0.9.1

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -5,6 +5,7 @@ from aiogram.types import ChatType
 import lib
 from assets import commands, texts
 from core import dp
+from loader import db
 
 
 @dp.command(commands.START, state='*')
@@ -18,3 +19,5 @@ async def _(msg: types.Message, state: FSMContext):
 
     if lib.is_admin(msg.from_user):
         await commands.setup()
+
+    db.save_chat(msg.chat.id)
