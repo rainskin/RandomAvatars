05875861261cc592f320f6f0e3fcd66c9bab2765
levch <levchenko.d.a1998@gmail.com>
2023-02-21 15:22:05 +0600

    v 1.5

--- bot/src/lib/events.py
+++ bot/src/lib/events.py
@@ -6,14 +6,5 @@ from core import dp
 start = dp.command(commands.START, state='*')
 text = dp.text()
 
-_PICTURE_COMMANDS = [
-    commands.GET_AVATARS,
-    commands.GET_PAIRED,
-    commands.GET_CUTE,
-    commands.GET_ANGRY,
-    commands.SEND_PICTURE,
-]
-
-get_picture = dp.command(_PICTURE_COMMANDS)
 send_picture = dp.command(commands.SEND_PICTURE)
 bad_request = dp.errors_handler(exception=BadRequest)
