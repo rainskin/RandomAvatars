74db6fb7724a41d7e043726b2bc01457cfe3d71f
levch <levchenko.d.a1998@gmail.com>
2023-02-22 07:37:54 +0600

    v 1.9.1

--- bot/src/lib/__init__.py
+++ bot/src/lib/__init__.py
@@ -27,10 +27,10 @@ def contain_trigger_words(text: str, trigger_words: list[str]) -> bool:
 
 
 TRIGGERS_TO_CATEGORY = [
-    (config.TextTriggers.AVATAR + [commands.GET_AVATARS], PictureCategory.AVATAR),
-    (config.TextTriggers.PAIRED_AVATARS + [commands.GET_PAIRED], PictureCategory.PAIRED_AVATARS),
-    (config.TextTriggers.CUTE_PICTURE + [commands.GET_CUTE], PictureCategory.CUTE),
-    (config.TextTriggers.ANGRY_PICTURE + [commands.GET_ANGRY], PictureCategory.ANGRY),
+    (config.TriggerWords.AVATAR + [commands.GET_AVATARS], PictureCategory.AVATAR),
+    (config.TriggerWords.PAIRED_AVATARS + [commands.GET_PAIRED], PictureCategory.PAIRED_AVATARS),
+    (config.TriggerWords.CUTE_PICTURE + [commands.GET_CUTE], PictureCategory.CUTE),
+    (config.TriggerWords.ANGRY_PICTURE + [commands.GET_ANGRY], PictureCategory.ANGRY),
 ]
 
 
@@ -67,15 +67,16 @@ def _choose_picture_category_message(request: MESSAGE) -> PictureCategory | None
 def _choose_picture_category_query(request: QUERY) -> PictureCategory | None:
     kb = kbs.MainMenu
 
-    match request.data:
-        case kb.anime_avatars.data:
-            return PictureCategory.AVATAR
-        case kb.paired_avatars.data:
-            return PictureCategory.PAIRED_AVATARS
-        case kb.cute_pictures.data:
-            return PictureCategory.CUTE
-        case kb.angry_pictures.data:
-            return PictureCategory.ANGRY
+    button_to_category = [
+        (kb.anime_avatars, PictureCategory.AVATAR),
+        (kb.paired_avatars, PictureCategory.PAIRED_AVATARS),
+        (kb.cute_pictures, PictureCategory.CUTE),
+        (kb.angry_pictures, PictureCategory.ANGRY),
+    ]
+
+    for button, category in button_to_category:
+        if request.data == button.data:
+            return category
 
 
 async def get_picture_category(for_user: USER) -> PictureCategory | None:
