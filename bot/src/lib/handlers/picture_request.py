746045be5ef41711f51588881d8ee3ab3212ecf9
levch <levchenko.d.a1998@gmail.com>
2023-03-18 00:33:02 +0600

    v 0.2dev

--- bot/src/lib/handlers/picture_request.py
+++ bot/src/lib/handlers/picture_request.py
@@ -1,24 +1,37 @@
 from core import handlers
-from lib.api import api
 from lib.assets import PictureCategory, Texts
-from lib.helpers import save_chat, get_cooldown, get_picture, set_cooldown
+from lib.helpers import *
 
 
 class PictureRequest(handlers.Query):
-    category: str = None
+    category: PictureCategory = None
+
+    def pick_category(self) -> PictureCategory | None:
+        raise NotImplementedError()
+
+    async def on_success(self):
+        pass
 
     async def callback(self):
         await save_chat(self.chat)
-        category = self.category = self.pick_category()
+        self.category = self.pick_category()
 
-        if category:
-            await self.check_rights()
-            await self.send()
-            await self.on_success()
+        if not self.category:
+            return
 
-    async def check_rights(self):
-        await self.check_required_join()
+        await self._check_rights()
+        await self._reply_picture()
+        await self.on_success()
+
+    async def _check_rights(self):
         await self.check_cooldown()
+        if self.is_chat_private:
+            await self.check_required_join()
+
+    async def _reply_picture(self):
+        picture = await get_picture(self.category, self.chat)
+        await self.reply_photos(picture)
+        await set_cooldown(self.user)
 
     async def check_cooldown(self):
         cooldown = await get_cooldown(self.user, self.chat)
@@ -30,32 +43,14 @@ class PictureRequest(handlers.Query):
         await self.cancel(text)
 
     async def check_required_join(self):
-        chat_id = await self.get_required_chat_id()
+        chat_id = await get_required_join()
 
         if not chat_id:
+            return
+
+        if await self.is_chat_member(chat_id):
             return
 
         link = await self.obtain_invite_link(chat_id)
         text = Texts.required_join.format(link)
         await self.cancel(text)
-
-    async def send(self):
-        picture = await get_picture(self.category, self.chat)
-        await self.reply_photos(picture)
-        await set_cooldown(self.user)
-
-    async def get_required_chat_id(self) -> int | None:
-        if not self.is_chat_private:
-            return None
-
-        if not (chat_id := await api.required_join.get_chat_id()):
-            return None
-
-        if not await self.is_chat_member(chat_id):
-            return chat_id
-
-    def pick_category(self) -> PictureCategory | None:
-        raise NotImplementedError()
-
-    async def on_success(self):
-        pass
--- bot/src/lib/helpers.py
+++ bot/src/lib/helpers.py
@@ -2,6 +2,14 @@ from telegram import Chat, User
 
 from .api import api
 
+___all__ = [
+    'save_chat',
+    'get_cooldown',
+    'get_picture',
+    'set_cooldown',
+    'get_required_join',
+]
+
 
 def save_chat(chat: Chat):
     return api.chats.save(chat.id)
@@ -11,9 +19,13 @@ def get_cooldown(user: User, chat: Chat):
     return api.user(user.id).cooldown.get(chat.type)
 
 
-def get_picture(category: str, chat: Chat):
-    return api.pictures(category).get_random(chat.id)
+async def get_picture(category: str, chat: Chat) -> list[str]:
+    return await api.pictures(category).get_random(chat.id)
 
 
 def set_cooldown(user: User):
     return api.user(user.id).cooldown.set()
+
+
+async def get_required_join() -> int:
+    return await api.required_join.get_chat_id()
